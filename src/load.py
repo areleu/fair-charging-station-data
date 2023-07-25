18ca8fb610afe0866e50b543969bc6c630dcd84a
Eugenio Salvador Arellano Ruiz <eugenio.arellanoruiz@dlr.de>
2023-07-25 10:42:38 +0200

    Renamed download to load

---
+++ src/load.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 German Aerospace Center (DLR)
+# SPDX-License-Identifier: BSD-3-Clause
+
+import requests
+from datetime import datetime
+from os import path, mkdir
+
+FILENAME = "bnetza_charging_stations_raw_{MM}_{YYYY}.xlsx"
+DATA_DIRECTORY = "data"
+
+
+def get_raw(dd_mm_yyyy: tuple = None):
+    if dd_mm_yyyy is None:
+        _, mm, yyyy = datetime.today().strftime("%d-%m-%Y").split("-")
+    else:
+        _, mm, yyyy = dd_mm_yyyy
+    file_path = path.join(DATA_DIRECTORY, FILENAME.format(MM=mm, YYYY=yyyy))
+    if not path.exists(DATA_DIRECTORY):
+        mkdir(DATA_DIRECTORY)
+    if not path.exists(file_path):
+        raise IOError(f"You have to manually download the file from the BNetzA webpage(see README) and save it as: {file_path}")
+    return file_path
+
+
+def main():
+    get_raw()
+
+
+if __name__ == "__main__":
+    main()
